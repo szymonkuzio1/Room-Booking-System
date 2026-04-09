ac4a019c44bc492ce18421dccd33be0e5825adaa
Patrique2oo1 <101894431+Patrique2oo1@users.noreply.github.com>
2026-04-09 12:42:39 +0200

    Kamień milowy 1 - optymalizacja API

--- KM1/serwer/src/main/java/pl/kregiel/kuzio/psk/projekt/dto/Rezerwacja_Request.java
+++ KM1/serwer/src/main/java/pl/kregiel/kuzio/psk/projekt/dto/Rezerwacja_Request.java
@@ -2,24 +2,16 @@ package pl.kregiel.kuzio.psk.projekt.dto;
 
 // klasa zawierająca informacje niezbędne do utworzenia rezerwacji
 public class Rezerwacja_Request {
-    private int sala;
     private String data_rezerwacji;
     private String poczatek_rezerwacji;
     private String koniec_rezerwacji;
 
-    public Rezerwacja_Request(int sala, String data_rezerwacji, String poczatek_rezerwacji, String koniec_rezerwacji) {
-        this.sala = sala;
+    public Rezerwacja_Request(String data_rezerwacji, String poczatek_rezerwacji, String koniec_rezerwacji) {
         this.data_rezerwacji = data_rezerwacji;
         this.poczatek_rezerwacji = poczatek_rezerwacji;
         this.koniec_rezerwacji = koniec_rezerwacji;
     }
 
-    public int getSala() {
-        return sala;
-    }
-    public void setSala(int sala) {
-        this.sala = sala;
-    }
     public String getData_rezerwacji() {
         return data_rezerwacji;
     }
