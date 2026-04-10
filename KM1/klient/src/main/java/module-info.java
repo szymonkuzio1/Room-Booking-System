c0ab1cabf83fb5cfa2ee98efa80a9fdc115dbfd5
Patrique2oo1 <101894431+Patrique2oo1@users.noreply.github.com>
2026-04-10 13:10:36 +0200

    Kamień milowy 1 - porządkowanie repozytorium

--- KM1/klient/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pl.kregiel.kuzio.psk.projekt {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.net.http;
-
-
-    opens pl.kregiel.kuzio.psk.projekt to javafx.fxml;
-    exports pl.kregiel.kuzio.psk.projekt;
-}
